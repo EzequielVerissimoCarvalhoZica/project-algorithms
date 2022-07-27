6616eda2a657db615d731a8d326fdbbe3598d86f
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-27 15:08:59 -0300

    style: flake8 remove empty lines

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period: list, target_time):
     if not (isinstance(target_time, int)):
         return None
-    
+
     counter = 0
     for login, logout in permanence_period:
         if not isinstance(login, int) or not isinstance(logout, int):
@@ -9,5 +9,5 @@ def study_schedule(permanence_period: list, target_time):
 
         if login <= target_time <= logout:
             counter += 1
-            
+
     return counter
