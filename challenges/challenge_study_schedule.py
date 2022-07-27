21e144c414b8e26a493086025b131f270b18210e
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-27 10:48:11 -0300

    feat: algorithm get number students studying at the same time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
-def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+def study_schedule(permanence_period: list, target_time):
+    if not (isinstance(target_time, int)):
+        return None
+    
+    counter = 0
+    for login, logout in permanence_period:
+        if not isinstance(login, int) or not isinstance(logout, int):
+            return None
+
+        if login <= target_time <= logout:
+            counter += 1
+            
+    return counter
