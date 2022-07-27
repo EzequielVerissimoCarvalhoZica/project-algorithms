294797364cda561716948241529bcd2208e1a741
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-27 12:25:01 -0300

    feat: algorithm palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,18 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    if len(word) == 1:
+        return True
+    
+    if high_index == 0:
+        return word[low_index]
+    
+    newWord = word[high_index] + is_palindrome_recursive(word, low_index, high_index - 1)
+    
+    if newWord == word:
+        return True
+    elif len(newWord) == len(word):
+        return False
+
+    return newWord
