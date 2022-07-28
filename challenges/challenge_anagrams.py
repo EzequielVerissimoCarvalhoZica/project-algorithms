4431f4f8a67c8ea2fd2e6f0f7bd95b114adc1cfa
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-28 15:15:24 -0300

    refactor: change logic

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,61 +1,15 @@
-# def merge_sort(list):
-#     if len(list) <= 1:
-#         return list
+def selection_sort(input_list):
+    length = len(input_list)
+    for index in range(length):
+        min_index = index
 
-#     mid = len(list) // 2
-#     left, right = merge_sort(list[:mid]), merge_sort(list[mid:])
+        for x in range(index + 1, length):
+            if input_list[x] < input_list[min_index]:
+                min_index = x
 
-#     return merge(left, right, list.copy())
+        input_list[min_index], input_list[index] = input_list[index], input_list[min_index]
 
-
-# def merge(left, right, merged):
-#     left_side, right_side = 0, 0
-
-#     while left_side < len(left) and right_side < len(right):
-#         if left[left_side] <= right[right_side]:
-#             merged[left_side + right_side] = left[left_side]
-#             left_side += 1
-#         else:
-#             merged[left_side + right_side] = right[right_side]
-#             right_side += 1
-
-#     for left_side in range(left_side, len(left)):
-#         merged[left_side + right_side] = left[left_side]
-
-#     for right_side in range(right_side, len(right)):
-#         merged[left_side + right_side] = right[right_side]
-
-#     return merged
-
-def partition(array, start, end):
-    pivot = array[start]
-    low = start + 1
-    high = end
-
-    while True:
-        while low <= high and array[high] >= pivot:
-            high = high - 1
-
-        while low <= high and array[low] <= pivot:
-            low = low + 1
-
-        if low <= high:
-            array[low], array[high] = array[high], array[low]
-        else:
-            break
-
-    array[start], array[high] = array[high], array[start]
-
-    return high
-
-
-def quick_sort(array, start, end):
-    if start >= end:
-        return
-
-    p = partition(array, start, end)
-    quick_sort(array, start, p-1)
-    quick_sort(array, p+1, end)
+    return input_list
 
 
 def split(word):
@@ -63,13 +17,12 @@ def split(word):
 
 
 def is_anagram(first_string: str, second_string: str):
-    if first_string == "" or second_string == "":
-        return False
-
     first_splited = split(first_string.lower())
     second_splied = split(second_string.lower())
 
-    quick_sort(first_splited, 0, len(first_splited) - 1)
-    quick_sort(second_splied, 0, len(first_splited) - 1)
+    first = selection_sort(first_splited)
+    second = selection_sort(second_splied)
 
-    return first_splited == second_splied
+    return first == second
+
+is_anagram("acd", "xca")
